f5f0ecce9c84ca6f0ddbd50eb4b78eff25583215
Rohan Khode <57560855+rohankhode18@users.noreply.github.com>
2025-07-18 14:43:45 +0530

    Create docker_build.groovy

---
+++ vars/docker_build.groovy
@@ -0,0 +1,5 @@
+def call(String ProjectName, String ImageTag, String DockerHubUser){
+  echo "Building Docker image"
+  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+  echo "Successfully Build Docker image"
+}
